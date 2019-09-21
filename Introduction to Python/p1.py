31b7722b038ba585c0102153c43d7f1983dc4da8
Mayank Priy <51355013+May-Beu@users.noreply.github.com>
2019-09-21 09:08:27 +0530

    Add files via upload

---
+++ Introduction to Python/p1.py
@@ -0,0 +1,8 @@
+'''
+Create a program that displays your name and complete  address formatted in
+the manner that you would usually see it on the outside of an envelope
+'''
+print("Mayank Priy")
+print("Sector 15")
+print('Noida,Gautam Buddha nagar')
+print('Uttar Pradesh-201301')
