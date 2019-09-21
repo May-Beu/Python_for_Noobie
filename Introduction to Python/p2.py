83ed9a5bbe98a91e2057f1715b78bd3d32776422
Mayank Priy <51355013+May-Beu@users.noreply.github.com>
2019-09-21 09:06:13 +0530

    Delete p2.py

--- Introduction to Python/p2.py
+++
@@ -1,6 +0,0 @@
-'''
-Write a program that asks the user to enter his or her name. The program should
-respond with a message that says hello to the user, using his or her name.
-'''
-name=input(" Enter your name :")
-print("Hello ",name)
