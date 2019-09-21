f6beb67de49b895689a90366a571094597f659b1
Mayank Priy <51355013+May-Beu@users.noreply.github.com>
2019-09-21 09:05:29 +0530

    Area of room

---
+++ Introduction to Python/p3.py
@@ -0,0 +1,9 @@
+'''
+Write a program that asks the user to enter the width and length of a room. Once
+the values have been read, your program should compute and display the area of the
+room. The length and the width will be entered as floating point numbers.
+'''
+width=float(input(" Enter the Width of room: "))
+length=float(input("Enter the length of room :"))
+area= width*length
+print("Area of room: ",area,"m") #m- meter
